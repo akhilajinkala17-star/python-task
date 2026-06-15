7d2e3513ccb9ad6c0b1ab48eae06342e783880c2
akhilajinkala17-star <akhilajinkala17@gmail.com>
2026-06-15 08:03:36 -0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,31 @@
+# 1.
+a=["Hydrabad", "banglore", "Chennai"]
+print(a)
+# 2.
+a=["nellore", "thirupathi", "chennai", "hydrabad", "pune"]
+print(a[2])
+# 3.
+a=["nellore", "thirupathi", "chennai", "hydrabad", "pune"]
+print(a[0:3])
+# 4.
+cities=["nellore", "thirupathi", "chennai", "hydrabad", "pune"]
+cities.sort()
+print(cities)
+# 5.
+a=["Hydrabad", "banglore", "Chennai"]
+a.append("thirupathi")
+print(a)
+# 6.
+a=["nellore", "thirupathi", "chennai", "hydrabad", "pune"]
+a.remove("nellore")
+print(a)
+# 7.
+def city_lists_len(cities):
+    return (len(cities))
+cities=["hydrabad", "nellore", "benglore", "ongole"]
+print(city_lists_len(cities))
+# 8.
+a=["Hydrabad", "banglore", "Chennai"]
+print(len(a))
+
+    
